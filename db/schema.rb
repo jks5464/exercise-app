be180bc44738b83bcc80f91b04454c8b5248f03b
EC2 Default User <ec2-user@ip-172-31-24-191.ec2.internal>
2019-01-08 03:56:32 +0000

    Minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108004653) do
+ActiveRecord::Schema.define(version: 20190108035224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "link", force: :cascade do |t|
     t.string "url",  limit: 255, null: false
