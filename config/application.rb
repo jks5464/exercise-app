8eb071b738a26d34a1449afb56eb379f8f1e1948
steerzac <steerzac@gmail.com>
2019-01-09 12:20:32 +0000

    Added rspec for user model.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module HangpersonRails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
     factory :user do
-        @new_user = Faker::Omniauth.unique.google
+        new_user = Faker::Omniauth.google
         uid { Faker::Number.unique.number(5) }
-        provider { @new_user.provider }
-        name { @new_user.name }
-        oauth_token { @new_user.credentials.token }
-        oauth_expires_at { @new_user.credentials.expires_at }
+        provider { "google_oauth2" }
+        name { new_user[:name] }
+        oauth_token { new_user[:credentials][:token] }
+        oauth_expires_at { 0 }
     end
 end
