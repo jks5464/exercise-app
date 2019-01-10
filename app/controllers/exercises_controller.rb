6b1374057af82d80b610fa8af0d022c7aed2de05
steerzac <steerzac@gmail.com>
2019-01-10 05:47:52 +0000

    Removed db session code from exercises_controller.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,24 +1,4 @@
 class ExercisesController < ApplicationController
-
-  before_action :get_db_from_session
-  after_action  :store_db_in_session
-  
-  private
-  
-  def get_db_from_session
-
-    @db = Database.new()
-    
-    if !session[:db].blank?
-      @db = YAML.load(session[:db])
-    end
-  end
-
-  def store_db_in_session
-
-    session[:db] = @db.to_yaml
-  end
-  
   public
   
   def new
