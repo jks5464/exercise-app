55a7f7bca32ae98346cad5fc6d533ee065538dae
EC2 Default User <ec2-user@ip-172-31-24-191.ec2.internal>
2019-01-27 21:29:21 +0000

    Minor

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '812103217807-vh3o74lbajgcujtt8jc6ojcsdblttql0.apps.googleusercontent.com', 'NXR9Lqn0dDx-ITbAQF31SOXT', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
+
+OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
