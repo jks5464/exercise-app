8aab649a4322aa14a32fe157b28110f49e3ff178
Constantina Hug <constantina.hug@gmail.com>
2019-01-30 03:49:18 +0000

    Displays completed exercises

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -32,9 +32,40 @@ class HomepageController < AuthenticationController
   
   def quick_log
     puts("Displaying quick log page...")
-    puts("Displaying list of exercises")
     @exercises = Exercise.all
     @exercises = [] if (@exercises.nil?)
+    
+    puts("Displaying user's workouts...")
+    @current_user = current_user()
+    #@workouts = @current_user.workout
+    #@workouts = [] if (@current_user.workout.nil?)
+    
+    @workouts = @current_user.workout
+    puts("User workouts: #{@workouts.first.name}")
+    puts("!"*100)
+    @workouts = [] if (@workouts.nil?)
+    
+
+    @tasks = []
+    @workouts.each do |workout|
+      @current_tasks = workout.task
+      @current_tasks.each do |t|
+        @tasks.append(t)
+      end
+    end
+    @tasks = [] if (Task.all.nil?)
+    
+    @exercise_sets = []
+    @tasks.each do |task|
+      @current_exercise_sets = task.exercise_set
+      @current_exercise_sets.each do |e|
+        if (e.completed)
+          @exercise_sets.append(e)
+        end
+      end
+    end
+    @exercise_sets = [] if (ExerciseSet.all.nil?)
+    
   end
   
 end
