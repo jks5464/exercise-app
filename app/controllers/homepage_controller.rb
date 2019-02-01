3b3ad0cc42f1ae3fe3363a1fc4e02fb1f4e1a9a8
steerzac <steerzac@gmail.com>
2019-02-01 04:53:13 +0000

    Removed dead code.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -40,32 +40,6 @@ class HomepageController < AuthenticationController
     
     @workouts = @current_user.workout
     @workouts = [] if (@workouts.nil?)
-    
-    workout_name = "Workout_" + session[:user_id].to_s + "_" + Time.now.to_s
-    puts("!"*100)
-    puts(workout_name)
-
-
-    @tasks = []
-    @workouts.each do |workout|
-      @current_tasks = workout.task
-      @current_tasks.each do |t|
-        @tasks.append(t)
-      end
-    end
-    @tasks = [] if (Task.all.nil?)
-    
-    @exercise_sets = []
-    @tasks.each do |task|
-      @current_exercise_sets = task.exercise_set
-      @current_exercise_sets.each do |e|
-        if (e.completed)
-          @exercise_sets.append(e)
-        end
-      end
-    end
-    @exercise_sets = [] if (ExerciseSet.all.nil?)
-    
   end
   
   def process_new_quick_log
