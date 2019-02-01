7efb0044baa0a4a2734e6be94f9acc8c4e5edaed
EC2 Default User <ec2-user@ip-172-31-24-191.ec2.internal>
2019-02-01 19:43:22 +0000

    Fixing stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,7 @@
-Rails.application.config.assets.precompile += %w( chartkick.js )
+Rails.application.configure do
+    Rails.application.config.assets.precompile += %w[
+        create_workout.js
+        search_exercises.js
+        quick_log.js
+    ]
+end
