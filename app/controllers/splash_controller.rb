c97a57748cec91f209b77216840b2784fea4f773
steerzac <steerzac@gmail.com>
2019-02-25 05:51:19 +0000

    Fixed splash controller.:

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,14 +1,10 @@
 class SplashController < ApplicationController
     def splash_screen()
-        if current_effective_user.is_admin? then
+        if !current_effective_user.nil? and current_effective_user.is_admin? then
             redirect_to admin_path
-            
         elsif current_effective_user then 
             redirect_to '/dashboard'
-            
         end
-        
-
         
     end
     
