0ac972d056506be6da354b37be45d166677dee0a
steerzac <steerzac@gmail.com>
2019-02-25 04:14:05 +0000

    Modified session controller so we don't have to hard code user names

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     session[:effective_id] = user.id
     
-    if(user.name.to_s == 'GirlScout Cookie') then
+    if (user.id == 1) or (Rails.env.development? and user.id == 6) then
       redirect_to admin_path
     else
       redirect_to dashboard_path
