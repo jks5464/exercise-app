ddd7e5193c46d10e9b2123f7fe12530d010f5a95
steerzac <steerzac@gmail.com>
2019-02-25 05:17:05 +0000

    Fixed splash screen check for admin.

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,6 +1,6 @@
 class SplashController < ApplicationController
     def splash_screen()
-        if current_effective_user == "GirlScout Cookie" then
+        if current_effective_user.is_admin? then
             redirect_to admin_path
             
         elsif current_effective_user then 
