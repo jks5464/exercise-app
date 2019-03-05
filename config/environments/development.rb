71cf7138c7268908d65cb23913875def8c8183e8
steerzac <steerzac@gmail.com>
2019-03-05 01:25:07 +0000

    http requests are redirected to https requests.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Redirects http requests to https requests
+  config.force_ssl = true
 end
