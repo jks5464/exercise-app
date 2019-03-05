055b899b29894b14f4c109376556a65911f914d1
steerzac <steerzac@gmail.com>
2019-03-05 01:44:14 +0000

    Removed https redirecting from test and development environments. Was causing some cucumber tests to fail.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
-  # Redirects http requests to https requests
-  config.force_ssl = true
 end
